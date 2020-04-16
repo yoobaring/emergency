caa12785a4557808878a316a09a335fa0acb5697
yoobaring <yoo.2538baring88@gmail.com>
2020-04-16 17:00:55 +0700

    Add Action Collapsemenu styled-components

---
+++ fornend/src/components/navbar/CollapseMenu.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+import { useSpring, animated } from 'react-spring';
+
+const CollapseMenu = (props) => {
+  const { open } = useSpring({ open: props.navbarState ? 0 : 1 });
+
+  if (props.navbarState === true) {
+    return (
+      <CollapseWrapper style={{
+        transform: open.interpolate({
+          range: [0, 0.2, 0.3, 1],
+          output: [0, -20, 0, -200],
+        }).interpolate(openValue => `translate3d(0, ${openValue}px, 0`),
+      }}
+      >
+
+      </CollapseWrapper>
+    );
+  }
+  return null;
+};
+
+export default CollapseMenu;
+
