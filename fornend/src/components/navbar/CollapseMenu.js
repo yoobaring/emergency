8fd5267f24dc7afe01c7a4190b64b5062d65d032
yoobaring <yoo.2538baring88@gmail.com>
2020-04-16 17:02:38 +0700

    Add Styled animated and import styledcomponents

--- fornend/src/components/navbar/CollapseMenu.js
+++ fornend/src/components/navbar/CollapseMenu.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import styled from 'styled-components';
 
 import { useSpring, animated } from 'react-spring';
 
@@ -24,3 +24,33 @@ const CollapseMenu = (props) => {
 
 export default CollapseMenu;
 
+const CollapseWrapper = styled(animated.div)`
+  background: #2d3436;
+  position: fixed;
+  top: 4.5rem;
+  left: 0;
+  right: 0;
+`;
+
+const NavLinks = styled.ul`
+  list-style-type: none;
+  padding: 2rem 1rem 2rem 2rem;
+
+  & li {
+    transition: all 300ms linear 0s;
+  }
+
+  & a {
+    font-size: 1.4rem;
+    line-height: 2;
+    color: #dfe6e9;
+    text-transform: uppercase;
+    text-decoration: none;
+    cursor: pointer;
+
+    &:hover {
+      color: #fdcb6e;
+      border-bottom: 1px solid #fdcb6e;
+    }
+  }
+`;
