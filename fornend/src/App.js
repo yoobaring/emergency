00124e467c17b8e32ad3e6007e12c6ed343499fb
yoobaring <yoo.2538baring88@gmail.com>
2020-04-17 04:20:51 +0700

    Run Footer from child components

--- fornend/src/App.js
+++ fornend/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import Navbar from "./components/navbar/Navbar";
-
 import GlobalStyle from './styles/Global';
+import Footer from './components/Footer';
+import './App.css'
 
 class App extends Component {
   state = {
@@ -13,15 +14,21 @@ class App extends Component {
   }
 
   render() {
-
     return (
-      <>
+    
+        <div className="page-container">
+        <div className="content-wrap">
         <Navbar 
           navbarState={this.state.navbarOpen} 
           handleNavbar={this.handleNavbar}
         />
         <GlobalStyle />
-      </>
+        
+      </div>
+      <Footer />
+      </div>
+      
+      
     )
   }
 }
