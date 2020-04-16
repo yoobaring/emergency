69ca493925e2ee3b2da9f157db62f8b79bf35136
yoobaring <yoo.2538baring88@gmail.com>
2020-04-16 17:24:41 +0700

    Add linkAnimation

--- fornend/src/components/navbar/Navbar.js
+++ fornend/src/components/navbar/Navbar.js
@@ -5,3 +5,40 @@ import Brand from "./Brand";
 import BurgerMenu from "./BurgerMenu";
 import CollapseMenu from "./CollapseMenu";
 
+const Navbar = (props) => {
+  const barAnimation = useSpring({
+    from: { transform: 'translate3d(0, -10rem, 0)' },
+    transform: 'translate3d(0, 0, 0)',
+  });
+
+  const linkAnimation = useSpring({
+    from: { transform: 'translate3d(0, 30px, 0)', opacity: 0 },
+    to: { transform: 'translate3d(0, 0, 0)', opacity: 1 },
+    delay: 800,
+    config: config.wobbly,
+  });
+
+  return (
+    <>
+      <NavBar style={barAnimation}>
+        <FlexContainer>
+          <Brand />
+
+          <BurgerWrapper>
+            <BurgerMenu
+              navbarState={props.navbarState} 
+              handleNavbar={props.handleNavbar}
+            />
+          </BurgerWrapper>
+        </FlexContainer>
+      </NavBar>
+      <CollapseMenu 
+        navbarState={props.navbarState} 
+        handleNavbar={props.handleNavbar}
+      />
+   </>
+  )
+}
+
+export default Navbar
+
