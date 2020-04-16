3bb4aa6de3d29db9e72a092bfaf224f400fb6a8e
yoobaring <yoo.2538baring88@gmail.com>
2020-04-16 16:31:16 +0700

    add the Global.js and Navbar.js components

--- fornend/src/App.js
+++ fornend/src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react'
+import Navbar from "./components/navbar/Navbar";
 
-const App = () =>  {
-  return(
-    <div>
+import GlobalStyle from './styles/Global';
 
-    </div>
-  )
+class App extends Component {
+  state = {
+    navbarOpen: false
+  }
+
+  handleNavbar = () => {
+    this.setState({ navbarOpen: !this.state.navbarOpen });
+  }
+
+  render() {
+
+    return (
+      <>
+        <Navbar 
+          navbarState={this.state.navbarOpen} 
+          handleNavbar={this.handleNavbar}
+        />
+        <GlobalStyle />
+      </>
+    )
+  }
 }
 
-export default App;
+export default App
