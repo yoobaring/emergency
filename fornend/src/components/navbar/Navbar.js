537b83f5fd96cf29ae332fad0077559f6838b9b8
yoobaring <yoo.2538baring88@gmail.com>
2020-04-16 17:26:15 +0700

    add styled Menu Bar Navbar.js

--- fornend/src/components/navbar/Navbar.js
+++ fornend/src/components/navbar/Navbar.js
@@ -47,3 +47,55 @@ const Navbar = (props) => {
 
 export default Navbar
 
+const NavBar = styled(animated.nav)`
+  position: fixed;
+  width: 100%;
+  top: 0;
+  left: 0;
+  background: #2d3436;
+  z-index: 1;
+  font-size: 1.4rem;
+`;
+
+const FlexContainer = styled.div`
+  max-width: 120rem;
+  display: flex;
+  margin: auto;
+  padding: 0 2rem;;
+  justify-content: space-between;
+  height: 5rem;
+`;
+
+const NavLinks = styled(animated.ul)`
+  justify-self: end;
+  list-style-type: none;
+  margin: auto 0;
+
+  & a {
+    color: #dfe6e9;
+    text-transform: uppercase;
+    font-weight: 600;
+    border-bottom: 1px solid transparent;
+    margin: 0 1.5rem;
+    transition: all 300ms linear 0s;
+    text-decoration: none;
+    cursor: pointer;
+
+    &:hover {
+      color: #fdcb6e;
+      border-bottom: 1px solid #fdcb6e;
+    }
+
+    @media (max-width: 768px) {
+      display: none;
+    }
+  }
+`;
+
+const BurgerWrapper = styled.div`
+  margin: auto 0;
+
+  @media (min-width: 769px) {
+    display: none;
+  }
+`;
