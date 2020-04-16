36b05f4eb6cf37ac73570c9e432367275fa876eb
yoobaring <yoo.2538baring88@gmail.com>
2020-04-17 04:09:46 +0700

    Create a footer ready to export

---
+++ fornend/src/components/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+
+const Footer = () => {
+
+    return(
+        <div className="main-footer">
+            <div className="container">
+                <div className="row">
+                    {/*Column1*/}
+                    <h1>Footer</h1>
+                    <div className="col">
+                        <h4>Thicc memes tnc</h4>
+                        <ul className="list-unstyled">
+                            <li>095296788</li>
+                            <li>Suraiya Baring</li>
+                            <li>Phuket</li>
+                        </ul>
+                </div>
+              
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Footer;
