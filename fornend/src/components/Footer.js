6e0955455df55475a0aa878ac7f3611f7cb2d5a2
yoobaring <yoo.2538baring88@gmail.com>
2020-04-22 00:38:47 +0700

    Edit text Footer

--- fornend/src/components/Footer.js
+++ fornend/src/components/Footer.js
@@ -9,16 +9,8 @@ const Footer = () => {
             <div className="container">
                 <div className="row">
                     {/*Column1*/}
-                    <h1>Footer</h1>
-                    <div className="col">
-                        <h4>Thicc memes tnc</h4>
-                        <ul className="list-unstyled">
-                            <li>095296788</li>
-                            <li>Suraiya Baring</li>
-                            <li>Phuket</li>
-                        </ul>
-                </div>
-              
+                    <h1>Emergency</h1>
+
                 </div>
             </div>
         </div>
