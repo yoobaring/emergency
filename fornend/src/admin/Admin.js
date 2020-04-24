0901642179177e554d553f10357b02a8b2a8764c
yoobaring <yoo.2538baring88@gmail.com>
2020-04-24 08:51:24 +0700

    Create CRUD sections Admin

--- fornend/src/admin/Admin.js
+++ fornend/src/admin/Admin.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Admin, Resource } from 'admin-on-rest';
-import { PostList } from './posts';
+import {Delete ,Admin, Resource } from 'admin-on-rest';
+import {PostEdit ,PostCreate, PostList } from './posts';
 import loopbackRestClient from 'aor-loopback';
 
 const Ad = () => (
     <Admin restClient={loopbackRestClient('http://localhost:3000/api/')}>
-        <Resource name="posts" list={PostList} />
+        <Resource name="posts" list={PostList} create={PostCreate} edit={PostEdit} remove={Delete}/>
     </Admin>
 );
 
--- fornend/src/admin/posts.js
+++ fornend/src/admin/posts.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { List, Datagrid, TextField } from 'admin-on-rest';
+import {Edit, Create, List, Datagrid, TextField } from 'admin-on-rest';
+import {SimpleForm} from 'admin-on-rest/lib/mui/form';
+import {TextInput} from 'admin-on-rest/lib/mui/input';
+import {EditButton} from 'admin-on-rest/lib/mui/button';
 
 export const PostList = (props) => (
     <List {...props}>
@@ -7,6 +10,26 @@ export const PostList = (props) => (
             <TextField source="id" />
             <TextField source="title" />
             <TextField source="body" />
+            <EditButton/>
+            
         </Datagrid>
     </List>
+);
+
+export const PostCreate = (props) => (
+    <Create {...props}>
+        <SimpleForm>
+            <TextInput source="title"/>
+            <TextInput source="body"/>
+        </SimpleForm>
+    </Create>
+);
+
+export const PostEdit = (props) => (
+    <Edit {...props}>
+        <SimpleForm>
+            <TextInput source="title"/>
+            <TextInput source="body"/>
+        </SimpleForm>
+    </Edit>
 );
