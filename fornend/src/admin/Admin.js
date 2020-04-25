bd676c9cbfabf2662e3c6b0b619c8a43b509c720
yoobaring <yoo.2538baring88@gmail.com>
2020-04-26 01:06:30 +0700

    Launch components theme Admin.

--- fornend/src/admin/Admin.js
+++ fornend/src/admin/Admin.js
@@ -5,9 +5,11 @@ import {nursesEdit ,nursesCreate, nursesList ,
         physicianEdit ,physicianCreate, physicianList ,
         adjutantEdit ,adjutantCreate, adjutantList,
        } from './posts';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 const Ad = () => (
-    <Admin restClient={loopbackRestClient('http://localhost:3000/api/')}>
+    <Admin theme={getMuiTheme(darkBaseTheme)} restClient={loopbackRestClient('http://localhost:3000/api/')}>
         <Resource name="nurses" list={nursesList} create={nursesCreate} edit={nursesEdit} remove={Delete}/>
         <Resource name="physicians" list={physicianList} create={physicianCreate} edit={physicianEdit} remove={Delete}/>
         <Resource name="adjutants" list={adjutantList} create={adjutantCreate} edit={adjutantEdit} remove={Delete} />
